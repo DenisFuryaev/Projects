0bf92cd8852be05480b6ca234f3c469c2564d23a
Denis <denis.furyaev@gmail.com>
2022-07-28 09:38:44 +0400

    dockerfile added

--- AzureWebAPI/Controllers/AuthorizationController.cs
+++ AzureWebAPI/Controllers/AuthorizationController.cs
@@ -19,12 +19,7 @@ public class AuthorizationController : ControllerBase
         try
         {
             await Azure.ReadAzureParameters("AzureParameters.json");
-
-            // Client credentials:
-            // {
-            //    "ClientID": "e71a96b9-ff04-4367-93dd-01827236c9ae",
-            //    "ClientSecret": "TPZ8Q~iUEmExyQQGpGXcw.kSrVyJxuYCQS1yzcZ4"
-            // }
+            
             Azure.ReadClientCredentials(clientCredentials);
 
             await Azure.UpdateBearerToken();
